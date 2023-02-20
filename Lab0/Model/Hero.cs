f9134c19f52d5ce1d7f7dc22eed44cd2a11e2e44
WiemeJarne <jarne.wieme@telenet.be>
2023-02-20 11:22:46 +0100

    c#intro

---
+++ Lab0/Model/Hero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0.Model
+{
+    internal class Hero
+    {
+		private string _myNemisis;
+
+		public string MyNemisis
+		{
+			get {
+				if(string.IsNullOrWhiteSpace(_myNemisis)) return "(no name)";
+				return _myNemisis;
+			}
+			set {
+				if (value != null && value.Length > 10) throw new ArgumentException("naam mag niet langer dan 10 tekens zijn");
+				_myNemisis = value;
+			}
+		}
+
+		public string Name { get; set; }
+
+		public int Level {  get; set; }
+
+		public Hero(string name) : this(name, "no nemisis", 1)
+		{}
+
+		public Hero(string name, string myNemisis, int level)
+		{
+			this.Name = name;
+			this.Level = level;
+			this.MyNemisis = myNemisis;
+		}
+	}
+}
---
+++ Lab0/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab0.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero("Jarne");
+            Hero hero2 = new Hero("player2", "Jarne", 2);
+            Console.WriteLine(hero.MyNemisis); //get
+            hero.MyNemisis = "my nemisis"; //set
+            Console.WriteLine(hero.MyNemisis); //get
+
+            Console.ReadLine();
+        }
+    }
+}
